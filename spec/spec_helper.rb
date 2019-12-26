6b0c484d21b7581bdfd6935a9aa9226dd56fdb5b
Maxime Loliée <maxime@siliadev.com>
2019-12-26 21:31:25 +0100

    fix(travis): add brew's bin path to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,5 @@ require 'serverspec'
 require 'tempfile'
 
 set :backend, :exec
-set :path, '/usr/local/go/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH'
+set :path, '~/.homebrew/bin:/usr/local/go/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH'
 set :request_pty, true
