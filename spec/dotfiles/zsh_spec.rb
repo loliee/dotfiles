5834460a52e38a36f86782ee4861fd9d09b5744a
Maxime Loliée <maxime@siliadev.com>
2019-12-30 18:30:49 +0100

    test: add zsh-competions spec

---
+++ spec/dotfiles/zsh_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe file("#{ENV['HOME']}/.zsh/completion") do
+  it { should be_directory }
+end
+
+describe file("#{ENV['HOME']}/.zsh/completion/_docker") do
+  it { should be_file }
+end
+
+describe file("#{ENV['HOME']}/.zsh/completion/_fly") do
+  it { should be_file }
+end
+
+describe file("#{ENV['HOME']}/.zsh/completion/travis.sh") do
+  it { should be_file }
+end
