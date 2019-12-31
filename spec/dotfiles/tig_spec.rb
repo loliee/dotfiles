95d706823b7413d2c7cbf4005f624205f1ea2aee
Maxime Loliée <maxime@siliadev.com>
2019-12-31 10:31:13 +0100

    feat(tig): add edit binding

--- spec/dotfiles/tig_spec.rb
+++ spec/dotfiles/tig_spec.rb
@@ -4,6 +4,7 @@ describe file("#{ENV['HOME']}/.tigrc") do
   it { should be_file }
   it { should contain("set main-view = line-number:no,interval=5 id:yes date:default author:full commit-title:yes,graph,refs,overflow=no") }
   it { should contain('set refresh-mode = after-command') }
+  it { should contain('bind main e edit') }
   it { should contain('bind main F !git cf %(commit)') }
   it { should contain('bind main R !git ri %(commit)^') }
   it { should contain('bind main A !git a %(file)') }
