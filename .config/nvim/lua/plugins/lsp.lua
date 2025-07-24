c1a0c3182c3f977e18e230f9b6960b079d0b283f
Maxime Loliée <maxime@siliadev.com>
2025-07-24 23:01:30 +0200

    Remove kickstart references in nvim configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -28,7 +28,7 @@ return {
   },
   config = function()
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
       callback = function(event)
         local map = function(keys, func, desc, mode)
           mode = mode or "n"
