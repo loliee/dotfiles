08d036faa46ad3016021b9f09712229f3468d5b5
Maxime Loliée <maxime@siliadev.com>
2025-07-24 23:01:30 +0200

    Remove kickstart references in nvim configs

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
