8ae9c8a58e82136f70b469155ce35d541165e008
Maxime Loliée <maxime@siliadev.com>
2025-09-28 21:30:27 +0200

    Remove nvim-lspconfig legacy framework
    Cf. https://github.com/neovim/nvim-lspconfig/tree/master?tab=readme-ov-file#important-%EF%B8%8F

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,29 +1,17 @@
 return {
-  -- Main LSP Configuration
   "neovim/nvim-lspconfig",
   lazy = false,
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    -- Mason must be loaded before its dependents so we need to set it up here.
-    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
     { "williamboman/mason.nvim", opts = {} },
-    "williamboman/mason-lspconfig.nvim",
+    { "williamboman/mason-lspconfig.nvim", opts = {} },
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     "b0o/schemastore.nvim",
-
-    -- Useful status updates for LSP.
     {
       "j-hui/fidget.nvim",
       opts = {
-        notification = {
-          window = {
-            winblend = 1, -- Background color opacity in the notification window
-          },
-        },
+        notification = { window = { winblend = 1 } },
       },
     },
-
-    -- Allows extra capabilities provided by nvim-cmp
     "saghen/blink.cmp",
   },
   config = function()
@@ -34,49 +22,19 @@ return {
           mode = mode or "n"
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
-
-        vim.cmd("highlight MasonLoading guifg=Red")
-
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-        -- Find references for the word under your cursor.
-        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-        -- Rename the variable under your cursor.
-        --  Most Language Servers support renaming across files, etc.
+        local tb = require("telescope.builtin")
+        map("gd", tb.lsp_definitions, "[G]oto [D]efinition")
+        map("gr", tb.lsp_references, "[G]oto [R]eferences")
+        map("gI", tb.lsp_implementations, "[G]oto [I]mplementation")
+        map("<leader>D", tb.lsp_type_definitions, "Type [D]efinition")
+        map("<leader>ds", tb.lsp_document_symbols, "[D]ocument [S]ymbols")
+        map("<leader>ws", tb.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
         map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
       end,
     })
 
-    -- Change diagnostic symbols in the sign column (gutter)
     if vim.g.have_nerd_font then
       local signs = { ERROR = "✗", WARN = "", INFO = "", HINT = "" }
       local diagnostic_signs = {}
@@ -84,79 +42,54 @@ return {
         diagnostic_signs[vim.diagnostic.severity[type]] = icon
       end
       vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●",
-          spacing = 4,
-        },
+        virtual_text = { prefix = "●", spacing = 4 },
         signs = { text = diagnostic_signs },
         underline = true,
         update_in_insert = false,
         severity_sort = true,
-        float = {
-          border = "rounded",
-          source = "always",
-        },
+        float = { border = "rounded", source = "always" },
       })
       vim.keymap.set("n", "<leader>z", function()
         vim.diagnostic.open_float({ border = "rounded" })
       end)
     end
 
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-    -- Enable the following language servers
     local servers = {
       ansiblels = {},
       ast_grep = {
-        default_config = {
-          cmd = { "ast-grep", "lsp" },
-          single_file_support = true,
-        },
+        cmd = { "ast-grep", "lsp" },
+        single_file_support = true,
       },
       bashls = {},
       dockerls = {},
       docker_compose_language_service = {
-        settings = {
-          telemetry = {
-            telemetryLevel = "off",
-          },
-        },
+        settings = { telemetry = { telemetryLevel = "off" } },
       },
       fish_lsp = {},
       jinja_lsp = {},
       jsonls = {
-        json = {
-          schemas = require("schemastore").json.schemas(),
-          validate = { enable = true },
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
         },
       },
       just = {},
       helm_ls = {},
       lua_ls = {
-        -- cmd = { ... },
-        -- filetypes = { ... },
-        -- capabilities = {},
         settings = {
           Lua = {},
         },
       },
       ruff = {},
       rust_analyzer = {},
-      -- harper_ls = {},
-      ["terraform-ls"] = {},
+      terraformls = {},
       yamlls = {
         settings = {
           yaml = {
-            customTags = {
-              "!reference sequence",
-            },
-            schemaStore = {
-              -- You must disable built-in schemaStore support if you want to use
-              -- this plugin and its advanced options like `ignore`.
-              enable = false,
-              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-              url = "",
-            },
+            customTags = { "!reference sequence" },
+            schemaStore = { enable = false, url = "" },
             schemas = require("schemastore").yaml.schemas({
               extra = {
                 {
@@ -172,32 +105,18 @@ return {
       },
     }
 
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      "stylua", -- Used to format Lua code
-      "rust_analyzer",
-      "jinja_lsp",
-    })
-    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-    for server_name, config in pairs(servers) do
-      vim.lsp.config(server_name, {
-        settings = config.settings or {},
-      })
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
+    for name, cfg in pairs(servers) do
+      local merged = vim.tbl_deep_extend("force", { capabilities = capabilities }, cfg or {})
+      vim.lsp.config(name, merged)
     end
 
-    ---@diagnostic disable-next-line
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, { "stylua" })
+    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
     require("mason-lspconfig").setup({
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-          require("lspconfig")[server_name].setup(server)
-        end,
-      },
+      ensure_installed = ensure_installed,
     })
   end,
 }
