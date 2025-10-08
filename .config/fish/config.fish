1c0340db2699b6bb4c9c18ae37e0659a59de4d10
Maxime Loliée <maxime@siliadev.com>
2025-10-08 08:32:38 +0200

    Remove useless redirections in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,32 +31,30 @@ abbr --add rms shred --remove
 abbr --add G --position anywhere --set-cursor "| rg %"
 abbr --add L --position anywhere --set-cursor "% | less"
 
-command -q aws &>/dev/null; and abbr --add aws3 aws s3api
-command -q direnv &>/dev/null; and abbr --add da direnv allow
+command -q aws; and abbr --add aws3 aws s3api
+command -q direnv; and abbr --add da direnv allow
 
-command -q rg &>/dev/null; and abbr --add rga rg --hidden --no-ignore
+command -q rg; and abbr --add rga rg --hidden --no-ignore
 
 # Docker
-command -q docker &>/dev/null; and abbr --add d docker
-command -q docker compose &>/dev/null; and abbr --add dc docker compose
+command -q docker; and abbr --add d docker
+command -q docker compose; and abbr --add dc docker compose
 
 # Kubernerntes
-command -q k9s &>/dev/null; and abbr --add k k9s
-command -q kubectl &>/dev/null; and abbr --add kb kubectl
-command -q kubens &>/dev/null; and abbr --add kn kubens
-command -q kubectx &>/dev/null; and abbr --add kx kubectx
+command -q k9s; and abbr --add k k9s
+command -q kubectl; and abbr --add kb kubectl
+command -q kubens; and abbr --add kn kubens
+command -q kubectx; and abbr --add kx kubectx
 
 # Git absorb
-if command -q git-absorb &>/dev/null
+if command -q git-absorb
     abbr --add ga git absorb
     abbr --add gaf git absorb --force
     abbr --add gar git absorb --and-rebase
     abbr --add garf git absorb --and-rebase
 end
 
-if command -q uv &>/dev/null
-    abbr --add pip uv pip
-end
+command -q uv; and abbr --add pip uv pip
 
 abbr fe --set-cursor=! "find . -name '*' -exec ! '{}' \;"
 abbr fed --set-cursor=! "find . -name '*' -type d -exec ! '{}' \;"
