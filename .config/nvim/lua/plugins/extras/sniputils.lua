3d33a0f278ad077ce61effd4bf0094236cc6306e
Maxime Loliée <maxime@siliadev.com>
2025-10-29 11:09:58 +0100

    Fix unused args in visual_insert sniputils

--- .config/nvim/lua/plugins/extras/sniputils.lua
+++ .config/nvim/lua/plugins/extras/sniputils.lua
@@ -4,7 +4,7 @@ local i = ls.insert_node
 local sn = ls.snippet_node
 local t = ls.text_node
 
-local visual_insert = function(args, parent)
+local visual_insert = function(_, parent)
   if #parent.snippet.env.LS_SELECT_RAW > 0 then
     return sn(nil, t(parent.snippet.env.LS_SELECT_RAW))
   else -- If LS_SELECT_RAW is empty, return a blank insert node
