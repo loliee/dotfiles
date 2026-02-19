3abc060e9459800c570f5ea85d139f56c604a398
Maxime Loliée <maxime@siliadev.com>
2026-02-19 15:38:47 +0100

    Remove nvim-treesitter.configs settings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return { -- Highlight, edit, and navigate code
   build = ":TSUpdate",
   lazy = false,
   priority = 1000,
-  main = "nvim-treesitter.configs", -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
